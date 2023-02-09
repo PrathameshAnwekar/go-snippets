327ce07ede6094aa2c7bc105e8096c79ca60676f
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-02-09 11:55:05 +0530

    get recordById working

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -11,7 +11,7 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	stmt :=  `INSERT INTO snippets (title, content, created, expires)
+	stmt := `INSERT INTO snippets (title, content, created, expires)
 VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
@@ -26,7 +26,22 @@ VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
 // This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 // This will return the 10 most recently created snippets.
